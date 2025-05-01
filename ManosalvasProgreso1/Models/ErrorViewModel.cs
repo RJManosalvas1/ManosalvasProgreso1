829a23423d3d5733bbc60e8803a8bd71c92db2f6
RJManosalvas1 <roberto.manosalvas@udla.edu.ec>
2025-05-01 07:16:12 -0500

    Agregar archivos de proyecto.

---
+++ ManosalvasProgreso1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManosalvasProgreso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
